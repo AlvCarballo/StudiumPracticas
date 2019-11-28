a710f1fcc7b9b5dc66891b71036923d2f095c479
AlvCarballo <48227234+AlvCarballo@users.noreply.github.com>
2019-11-28 13:24:49 +0100

    Update Traductor.java

--- trimestre1.Practica1/src/es/Studium/Trimestre1/Practica1/Traductor.java
+++ trimestre1.Practica1/src/es/Studium/Trimestre1/Practica1/Traductor.java
@@ -1,8 +1,8 @@
 package es.Studium.Trimestre1.Practica1;
 /*
- * Traductor - Realizar un programa que lea una frase en español y la traduzca al
-inglés o viceversa. Usaremos un diccionario reducido compuesto por las palabras
-yo, tu, el, ella, soy, eres, es, alto, bajo, español y sus equivalentes en inglés.
+ * Traductor - Realizar un programa que lea una frase en espaÃ±ol y la traduzca al
+inglÃ©s o viceversa. Usaremos un diccionario reducido compuesto por las palabras
+yo, tu, el, ella, soy, eres, es, alto, bajo, espaÃ±ol y sus equivalentes en inglÃ©s.
  */
 import java.util.Scanner;
 public class Traductor 
@@ -10,101 +10,115 @@ public class Traductor
 
 	public static void main(String[] args) 
 	{
-		// Le aplicamos a teclado la utilidad escáner
+		// Le aplicamos a teclado la utilidad escÃ¡ner
 		Scanner teclado = new Scanner(System.in);
 		String palabra;
-		
-		System.out.println("Atención, escriba todo en minúsculas");
+		System.out.println("AtenciÃ³n, escriba todo en minÃºsculas");
 		System.out.println("Indique una palabra a traducir:");
 		palabra=teclado.nextLine();//Leemos la cadena a traducir
-		traducir(palabra); //método para traducir
 		teclado.close(); //Cerramos teclado
+		String tabla[] = new String[palabra.length()];
+		String[] parts = palabra.split(" ");//split separa por lo que se le diga
+		separarFrase(tabla, parts);
+		System.out.println("La frase traducida es:");
+		revisarTraductor(tabla, parts);
 	}
-	private static void traducir(String palabra) {
-		switch (palabra)
+
+	private static void separarFrase(String[] tabla, String[] parts) {
+		for(int i=0;i<parts.length;i++)//recorremos tantas veces como palabras tenga la frase
 		{
-		case "yo":
-			System.out.println("La traducción a inglés es:");
-			System.out.println("i");
-			break;//detiene el switch
-		case "tu":
-			System.out.println("La traducción al inglés es:");
-			System.out.println("you");
-			break;//detiene el switch
-		case "el":
-			System.out.println("La traducción al inglés es:");
-			System.out.println("he");
-			break;//detiene el switch
-		case "ella":
-			System.out.println("La traducción al inglés es:");
-			System.out.println("she");
-			break;//detiene el switch
-		case "soy":
-			System.out.println("La traducción al inglés es:");
-			System.out.println("I am");
-			break;//detiene el switch
-		case "eres":
-			System.out.println("La traducción al inglés es:");
-			System.out.println("you are");
-			break;//detiene el switch
-		case "es":
-			System.out.println("La traducción al inglés es:");
-			System.out.println("is");
-			break;//detiene el switch
-		case "alto":
-			System.out.println("La traducción a inglés es:");
-			System.out.println("tall");
-			break;//detiene el switch
-		case "bajo":
-			System.out.println("La traducción a inglés es:");
-			System.out.println("low");
-			break;//detiene el switch
-		case "español":
-			System.out.println("La traducción a inglés es:");
-			System.out.println("spanish");
-			break;//detiene el switch
-		case "i":
-			System.out.println("La traducción a español es:");
-			System.out.println("yo");
-			break;//detiene el switch
-		case "you":
-			System.out.println("La traducción a español es:");
-			System.out.println("tu");
-			break;//detiene el switch
-		case "he":
-			System.out.println("La traducción a español es:");
-			System.out.println("el");
-			break;//detiene el switch
-		case "she":
-			System.out.println("La traducción a español es:");
-			System.out.println("ella");
-			break;//detiene el switch
-		case "i am":
-			System.out.println("La traducción a español es:");
-			System.out.println("soy");
-			break;//detiene el switch
-		case "you are":
-			System.out.println("La traducción a español es:");
-			System.out.println("eres");
-			break;//detiene el switch
-		case "is":
-			System.out.println("La traducción a español es:");
-			System.out.println("es");
-			break;//detiene el switch
-		case "tall":
-			System.out.println("La traducción a español es:");
-			System.out.println("alto");
-			break;//detiene el switch
-		case "low":
-			System.out.println("La traducción a español es:");
-			System.out.println("bajo");
-			break;//detiene el switch
-		case "spanish":
-			System.out.println("La traducción a español es:");
-			System.out.println("español");
-			break; //detiene el switch
-		default://Por defecto mostrara el siguiente mensaje de error
-			System.out.println("Introduzca una palabra que se encuentre en el diccionario, y todo en minúsculas");
+			tabla[i] = parts[i];//metemos las palabras en una tabla
+		}
+	}
+
+	private static void revisarTraductor(String[] tabla, String[] parts) {
+		for(int i=0;i<parts.length;i++)//recorremos tantas veces como palabras hay.
+		{
+			switch (tabla[i])//Miramos si las palabras almacenadas en la tabla y imprimimos el resultado
+			{
+			case "yo":
+				System.out.print("i");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "tu":
+				System.out.print("you");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "el":
+				System.out.print("he");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "ella":
+				System.out.print("she");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "soy":
+				System.out.print("am");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "eres":
+				System.out.print("are");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "es":
+				System.out.print("is");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "alto":
+				System.out.print("tall");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "bajo":
+				System.out.print("low");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "espaÃ±ol":
+				System.out.print("spanish");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "i":
+				System.out.print("yo");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "you":
+				System.out.print("tu");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "he":
+				System.out.print("el");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "she":
+				System.out.print("ella");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "i am":
+				System.out.print("soy");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "you are":
+				System.out.print("eres");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "is":
+				System.out.print("es");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "tall":
+				System.out.print("alto");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "low":
+				System.out.print("bajo");
+				System.out.print(" ");
+				break;//detiene el switch
+			case "spanish":
+				System.out.print("espaÃ±ol");
+				System.out.print(" ");
+				break; //detiene el switch
+			default://Por defecto mostrara el siguiente mensaje de error
+				System.out.println("Error, la palabra no se encuentra");
+			}
 		}
 	}
 }
